fd1f105b11c259f518d3454901021bd3e10ad19a
Abolfazl.ghaseemi <ariancode@yahoo.com>
2024-01-16 23:46:13 +0330

    finish task list

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,60 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:task_list/data.dart';
 
-const taskBoxName = 'task';
+const taskBoxName = 'tasks';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
   Hive.registerAdapter(PriorityAdapter());
-  await Hive.openBox<Task>(taskBoxName);
+  await Hive.openBox<TaskEntity>(taskBoxName);
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: primaryVariantColor));
   runApp(const MyApp());
 }
 
+const Color primaryColor = Color(0xff794CFF);
+const Color primaryVariantColor = Color(0xff5C0AFF);
+const secondaryTextColor = Color(0xffAFBED0);
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const primaryTextColor = Color(0xff1D2830);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          textTheme: GoogleFonts.poppinsTextTheme(const TextTheme(
+              headline6: TextStyle(fontWeight: FontWeight.bold))),
+          inputDecorationTheme: const InputDecorationTheme(
+              labelStyle: TextStyle(color: secondaryTextColor),
+              border: InputBorder.none,
+              iconColor: secondaryTextColor),
+          colorScheme: const ColorScheme.light(
+              primary: primaryColor,
+              primaryContainer: primaryVariantColor,
+              background: Color(0xffF3F5F8),
+              onSurface: primaryTextColor,
+              onPrimary: Colors.white,
+              onBackground: primaryTextColor,
+              secondary: primaryColor,
+              onSecondary: Colors.white)),
       home: const HomeScreen(),
     );
   }
@@ -53,9 +70,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final box = Hive.box<Task>(taskBoxName);
+    final box = Hive.box<TaskEntity>(taskBoxName);
+    final themeData = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(title: const Text('To Do List')),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.of(context)
@@ -63,21 +81,142 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         label: const Text('Add New Task'),
       ),
-      body: ValueListenableBuilder<Box<Task>>(
-          valueListenable: box.listenable(),
-          builder: (context, box, child) {
-            return ListView.builder(
-                itemCount: box.values.length,
-                itemBuilder: (context, index) {
-                  final Task task = box.values.toList()[index];
-                  return Container(
-                    child: Text(
-                      task.name,
-                      style: const TextStyle(fontSize: 24),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 102,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  themeData.colorScheme.primary,
+                  themeData.colorScheme.primaryContainer,
+                ]),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'To Do Llst',
+                        style: themeData.textTheme.headline6!
+                            .apply(color: Colors.white),
+                      ),
+                      Icon(
+                        CupertinoIcons.share,
+                        color: themeData.colorScheme.onPrimary,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    height: 38,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(19),
+                        color: themeData.colorScheme.onPrimary,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 20,
+                          )
+                        ]),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(CupertinoIcons.search),
+                        label: Text('search Tags...'),
+                      ),
                     ),
-                  );
-                });
-          }),
+                  ),
+                ]),
+              ),
+            ),
+            Expanded(
+              child: ValueListenableBuilder<Box<TaskEntity>>(
+                  valueListenable: box.listenable(),
+                  builder: (context, box, child) {
+                    return ListView.builder(
+                        padding: const EdgeInsets.fromLTRB(16, 16, 16, 100),
+                        itemCount: box.values.length + 1,
+                        itemBuilder: (context, index) {
+                          if (index == 0) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Today',
+                                      style: themeData.textTheme.headline6!
+                                          .apply(fontSizeFactor: 0.9),
+                                    ),
+                                    Container(
+                                        width: 100,
+                                        height: 3,
+                                        decoration: BoxDecoration(
+                                            color: primaryColor,
+                                            borderRadius:
+                                                BorderRadius.circular(1.5)))
+                                  ],
+                                ),
+                                MaterialButton(
+                                  color: Color(0xffEAEFF5),
+                                  textColor: secondaryTextColor,
+                                  elevation: 0,
+                                  onPressed: () {},
+                                  child: const Row(
+                                    children: [
+                                      Text('Delete All'),
+                                      SizedBox(
+                                        width: 4,
+                                      ),
+                                      Icon(CupertinoIcons.delete_solid)
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            );
+                          } else {
+                            final TaskEntity task =
+                                box.values.toList()[index - 1];
+                            return TaskItem(task: task);
+                          }
+                        });
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatelessWidget {
+  const TaskItem({
+    super.key,
+    required this.task,
+  });
+
+  final TaskEntity task;
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    return Container(
+      height: 84,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: themeData.colorScheme.surface,
+          boxShadow: [
+            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(0.2)),
+          ]),
+      child: Text(
+        task.name,
+        style: const TextStyle(fontSize: 24),
+      ),
     );
   }
 }
@@ -89,15 +228,17 @@ class EditTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Edit Task')),
+      appBar: AppBar(
+        title: const Text('Edit Task'),
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          final task = Task();
+          final task = TaskEntity();
           task.name = _controller.text;
           if (task.isInBox) {
             task.save();
           } else {
-            final Box<Task> box = Hive.box(taskBoxName);
+            final Box<TaskEntity> box = Hive.box(taskBoxName);
             box.add(task);
           }
           Navigator.of(context).pop();
